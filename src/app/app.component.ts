8a3ca5f3a97ee36a01caa3ba333184582a2e6985
PUSHPDEV PATEL <pushpdevpatel.in@gmail.com>
2024-01-23 16:28:00 +0530

    Almost Routing Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CommonModule, HeaderComponent, RouterOutlet, FooterComponent]
 })
 export class AppComponent {
   title = 'SRKP_Path_Lab';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ServicesComponent } from './services/services.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
-    
+    { 'path': 'home','title':'Home', component: HomeComponent },
+    { 'path': '',redirectTo:'/home', pathMatch:'full' },
+    { 'path': 'about','title':'About Us', component: AboutUsComponent },
+    { 'path': 'contact','title':'Contact Us', component: ContactUsComponent },
+    { 'path': 'service','title':'Our Services', component: ServicesComponent },
+    { 'path': '**','title':'Page Not Found', component: PageNotFoundComponent }
+
 ];
