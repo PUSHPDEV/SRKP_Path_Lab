5f4f1dd146ef7ce0b26f38911226b3cbf87ad19d
PUSHPDEV PATEL <pushpdevpatel.in@gmail.com>
2024-01-24 14:41:18 +0530

    w

--- src/app/images/images.component.ts
+++ src/app/images/images.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-images',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './images.component.html',
   styleUrl: './images.component.css'
 })
-export class ImagesComponent {
+export class ImagesComponent implements OnInit {
+  ngOnInit(): void {
+    this.fetchImages();
+  }
+
+  httpClient = inject(HttpClient);
+  images: any[] = [];
+
+  fetchImages() {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/posts/1').subscribe()
+  }
+
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private router:Router){}
+  loginObj: any = {
+    userName: '',
+    // userEmail:'',
+    password: ''
+  }
+  onLogin(){
 
+    if(this.loginObj.userName=="admin" && this.loginObj.password=="1806252"){
+this.router.navigateByUrl('/home')
+
+    }else{
+      alert('Wrong Credentials')
+    }
+  }
 }
