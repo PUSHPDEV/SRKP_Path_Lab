072b2befdb9b36020e6fdcc2237b6e4dc1b8b045
PUSHPDEV PATEL <pushpdevpatel.in@gmail.com>
2024-01-25 19:08:54 +0530

    u

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,14 @@ import { CommonModule } from '@angular/common';
 import { CarouselComponent } from "./carousel/carousel.component";
 import { AboutUsComponent } from "./about-us/about-us.component";
 import { ImagesComponent } from "./images/images.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, HeaderComponent, RouterOutlet, FooterComponent, CarouselComponent, AboutUsComponent, ImagesComponent]
+    imports: [CommonModule, HeaderComponent, RouterOutlet, FooterComponent, CarouselComponent, AboutUsComponent, ImagesComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'SRKP_Path_Lab';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { AboutUsComponent } from "../about-us/about-us.component";
 import { FooterComponent } from "../footer/footer.component";
+import { CarouselComponent } from "../carousel/carousel.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [AboutUsComponent, FooterComponent]
+    imports: [AboutUsComponent, FooterComponent, CarouselComponent]
 })
 export class HomeComponent {
 
