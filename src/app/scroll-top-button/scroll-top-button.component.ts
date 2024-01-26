617f67d3b328d912df48354fd65d312294f6d9b5
PUSHPDEV PATEL <pushpdevpatel.in@gmail.com>
2024-01-26 18:40:21 +0530

    ed

--- src/app/scroll-top-button/scroll-top-button.component.ts
+++ src/app/scroll-top-button/scroll-top-button.component.ts
@@ -10,7 +10,7 @@ import { Component, HostListener, Inject, OnInit } from '@angular/core';
 })
 export class ScrollTopButtonComponent implements OnInit {
 
-  windowScrolled: boolean | undefined;
+  windowScrolled: boolean = true;
 
   constructor(@Inject(DOCUMENT) private document: Document) { }
 
@@ -19,17 +19,17 @@ export class ScrollTopButtonComponent implements OnInit {
   onWindowScroll() {
     if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
       this.windowScrolled = true;
-    } 
-    else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) { 
+    }
+    else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
       this.windowScrolled = false;
     }
-  } 
-  
+  }
+
   scrollToTop() {
     (function smoothscroll() {
 
-      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop; 
-      
+      var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+
       if (currentScroll > 0) {
         window.requestAnimationFrame(smoothscroll);
         window.scrollTo(0, currentScroll - (currentScroll / 8));
